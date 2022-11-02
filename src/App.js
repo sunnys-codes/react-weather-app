9a97f6dfd7f0f94538189e3c2ca51c1c9b757971
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-11-02 11:23:42 -0400

    Added the Github Repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return <div className="App"> 
+  <h1>Weather App</h1>
+  <footer>
+    This project was coded by Sandra Thermildor and is {" "}
+  <a 
+    href="https://github.com/sunnys-codes/react-weather-app"
+    rel="noreferrer" target="_blank"
+  >
+
+  open-sourced on Github
+  </a>
+  </footer>
+   </div>;
+ 
+ 
 }
 
 
