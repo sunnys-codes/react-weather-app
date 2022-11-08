4aac31989e46d78f99caf998e0114f366feb9a3a
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-11-07 21:56:51 -0500

    Fixed search engine

--- src/Weather.js
+++ src/Weather.js
@@ -21,47 +21,44 @@ export default function Weather(props) {
 
     }
 
-    function search() {
-            
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
-
-    }
-
-    function handleSubmit(event){
+    function handleSubmit(event) {
         event.preventDefault();
         search();
-}
-
-    function handleCityChange(event) {
+      }
+    
+      function handleCityChange(event) {
         setCity(event.target.value);
-}
-    if (weatherData.ready) {
-        return (
-            <div className="Weather">
-           <form on Submit={handleSubmit}>
-                  <div className="row">
-                    <div className="col-9">
-                      <input
-                        type="search"
-                        placeholder="Enter a city.."
-                        className="form-control"
-                        autoFocus="on"
-                        onChange={handleCityChange}
-                    />
-                    </div>
-                    <div className="col-3">
-                      <input
-                        type="submit"
-                        value="Search"
-                        className="btn btn-primary w-100"
-                      />
-                    </div>
-                    </div>
-                </form>
-                <WeatherInfo data={weatherData} />
-                
+      }
+    
+      function search() {
+        const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+      }
+if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
                     </div>
         
             );
