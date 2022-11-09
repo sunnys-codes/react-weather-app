f064752cdc20e1900dcf4d434789b987ae496567
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-11-08 19:09:16 -0500

    Added WeatherInfo.css and fixed spacing

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,17 +14,15 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
+      <div class="row">
+              <div class="col-6">
+                <div class="weather-icon d-inline-flex p-2">
               <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div className="float-left">
-              <WeatherTemperature fahrenheit={props.data.temperature} />
-            </div>
-          </div>
+              </div>
+              <div class="weather-temperature col-4 float-start inline ">
+              <WeatherTemperature fahrenheit={props.data.temperature} /> 
+              
+              </div>
         </div>
         <div className="col-6">
           <ul>
